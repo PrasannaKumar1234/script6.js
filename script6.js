52790b1fe37116b7e9716e5729a5b331294ee35c
Prasanna Kumar <112098296+PrasannaKumar1234@users.noreply.github.com>
2022-10-14 16:23:10 +0530

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,77 @@
+
+//1)    Perform the following operations to provide the implementation for a
+//      Rectangle class. The operations are:
+
+
+/*
+Rectangle.prototype.area = function () {
+    return this.w * this.h;
+}
+
+class Square extends Rectangle {
+    constructor(w) {
+        super(w);
+
+        this.h = w;
+    }
+}
+*/
+
+//*********************************************************************************************************************************** 
+
+/*2)    Write a javascript function find_largest to return the nth largest number
+
+          in an array-*/
+
+
+/*
+
+function largestOfFour(arr) {
+    var largestNumber = [0];
+    for(var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
+     for(var subArrayIndex = 0; subArrayIndex < arr[arrayIndex].length; subArrayIndex++) {
+        if(arr[arrayIndex][subArrayIndex] > largestNumber[arrayIndex]) {         
+           largestNumber[arrayIndex] = arr[arrayIndex][subArrayIndex];
+         }
+     }
+  }
+  console.log(largestNumber);
+ }
+ largestOfFour([[4, 5, 1, 3]]);
+*/
+//********************************************************************************************************************************************************************* */
+
+
+//3) Write a JavaScript program which accept a number as input in the
+//function parameter and insert dashes (-) between each two even
+//numbers.
+
+
+
+// function BIN(){
+
+
+//     for(var n=2;n<=6;n++){
+// if(n%2===0){
+//     console.log(n,"-");
+// }  else{
+
+//     if(n%2!==0){
+//         console.log(n);
+//     }
+// }
+
+//     }
+// }BIN(023456);
+
+
+
+
+
+
+
+
+
+
+
+
